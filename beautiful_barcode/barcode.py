32ed5b5fe7be2cae7123f72c1d1e3b75db8ae063
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2023-01-19 16:47:07 +0100

    generate eps by default with .eps names
    `auto` sensibly defaults to SVG, but I generated SVG by accident multiple times.
    Generate eps for eps filenames.

--- beautiful_barcode/barcode.py
+++ beautiful_barcode/barcode.py
@@ -6,14 +6,14 @@ class Barcode:
         """ Convert the Barcode to modules (1 or 0) """
         raise NotImplementedError
 
-    def render(self, renderer='auto') -> bytes:
+    def render(self, renderer='auto', filename=None) -> bytes:
         """ Renders the barcode as a bytes object """
 
-        rend = renderers.make_renderer(renderer)
+        rend = renderers.make_renderer(renderer, filename=filename)
         self._paint(rend)
         return rend.to_bytes()
 
     def write(self, filename, renderer='auto'):
-        data = self.render(renderer=renderer)
+        data = self.render(renderer=renderer, filename=filename)
         with open(filename, 'wb') as out_file:
             out_file.write(data)
